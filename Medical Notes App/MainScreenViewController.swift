fd0b1a4a8ffc007d8fc2ffa7023ff9cff419f891
ars192 <amanzholov.arystan@gmail.com>
2020-12-15 05:05:06 +0600

    Added UI elements to Onboarding

--- Medical Notes App/MainScreenViewController.swift
+++ Medical Notes App/MainScreenViewController.swift
@@ -12,47 +12,73 @@ class MainScreenViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        let signupButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
-        signupButton.setTitle("Create Account", for: .normal)
-        signupButton.setTitleColor(.white, for: .normal)
-//        signupButton.titleLabel?.font = UIFont(name: "", size: <#T##CGFloat#>)
-        signupButton.layer.cornerRadius = 15
-        signupButton.layer.borderWidth = 1
-        signupButton.layer.borderColor = #colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1)
-        signupButton.addTarget(self, action: #selector(onButtonPressed(_:)), for: .touchUpInside)
-        signupButton.backgroundColor = #colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1)
-        
-        let loginButton = UIButton(frame: CGRect(x: 300, y: 300, width: 200, height: 200))
-        loginButton.setTitle("Sign In", for: .normal)
-        loginButton.setTitleColor(#colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1), for: .normal)
-        loginButton.backgroundColor = .white
-        loginButton.layer.cornerRadius = 15
-        loginButton.layer.borderWidth = 1
-        loginButton.layer.borderColor = #colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1)
-        loginButton.addTarget(self, action: #selector(onButtonPressed(_:)), for: .touchUpInside)
-        
         let logoImage = UIImage(named: "logo")
         
         let imageView = UIImageView()
         imageView.contentMode = UIView.ContentMode.scaleAspectFill
         imageView.image = logoImage
         
+        let name = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        name.text = "Medical\n Notes App"
+        name.font = UIFont.boldSystemFont(ofSize: 36)
+        name.textColor = #colorLiteral(red: 0.05098039216, green: 0.2470588235, blue: 0.4039215686, alpha: 1)
+        name.textAlignment = .center
+        name.numberOfLines = 2
+        name.lineBreakMode = .byTruncatingTail
+        
+        let description = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        description.text = "Make your doctor notes as easy\n as post a wish from your\n WantApp."
+        description.textColor = #colorLiteral(red: 0.05098039216, green: 0.2470588235, blue: 0.4039215686, alpha: 1)
+        description.textAlignment = .center
+        description.numberOfLines = 3
+        description.lineBreakMode = .byWordWrapping
+        
+        let signupButton = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        signupButton.setTitle("Create Account", for: .normal)
+        signupButton.setTitleColor(.white, for: .normal)
+        signupButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        signupButton.layer.cornerRadius = 15
+        signupButton.layer.borderWidth = 1
+        signupButton.layer.borderColor = #colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1)
+        signupButton.addTarget(self, action: #selector(onButtonPressed(_:)), for: .touchUpInside)
+        signupButton.backgroundColor = #colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1)
+        
+        let loginButton = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        loginButton.setTitle("Sign In", for: .normal)
+        loginButton.setTitleColor(#colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1), for: .normal)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        loginButton.backgroundColor = .white
+        loginButton.layer.cornerRadius = 15
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = #colorLiteral(red: 0.4196078431, green: 0.2823529412, blue: 1, alpha: 1)
+        loginButton.addTarget(self, action: #selector(onButtonPressed(_:)), for: .touchUpInside)
+        
+
+        
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 10
+        stackView.spacing = 20
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         stackView.addArrangedSubview(imageView)
         imageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
+        stackView.addArrangedSubview(name)
+//        name.widthAnchor.constraint(equalToConstant: 300).isActive = true
+//        name.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        stackView.addArrangedSubview(description)
+        description.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        description.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
         stackView.addArrangedSubview(signupButton)
-        signupButton.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        signupButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        signupButton.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        signupButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
         
         stackView.addArrangedSubview(loginButton)
-        loginButton.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        loginButton.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        loginButton.heightAnchor.constraint(equalToConstant: 75).isActive = true
         
         self.view.addSubview(stackView)
         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
